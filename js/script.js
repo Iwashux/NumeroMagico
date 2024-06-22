0622425354c45f8e72ca6029b7e1168df3165314
iwashux <edu.silva.vil@gmail.com>
2024-06-22 15:52:37 -0400

    Creacion del Proyecto

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ let numeroSecreto = Math.floor(Math.random() * numerosValidos)+1; // numero alea
 textoNumeros.textContent = `Ingresa un numero del 1 al ${numerosValidos}` 
 intentos.textContent = `Tienes ${posiblesIntentos} intentos`;
 
-// validacion del numero secreto
+// validacion por precionar Enter
 inputUsuario.addEventListener('keydown', function(event) {
     numeroUsuario = parseInt(this.value);
     // Comprobar si la tecla presionada es Enter
@@ -26,53 +26,63 @@ inputUsuario.addEventListener('keydown', function(event) {
     }
 });
 
+// validacion por precionar boton
 buttonUsuario.addEventListener('click', function() {
     numeroUsuario = parseInt(inputUsuario.value);
-    
+    // comprueba si el boton dice Enviar
     if(buttonUsuario.textContent === 'Enviar'){
         validarNumero();
-    }else{
+    }else{ // caso contrario se reinicia la pagina
         location.reload();
     }
 });
 
-function validarNumero() {
-    if (!isNaN(numeroUsuario)) {
-        if (numeroUsuario === numeroSecreto) {
+function validarNumero() { // validacion del numero secreto
+    if (!isNaN(numeroUsuario)) { // validacion de que el campo no este vacio
+        if (numeroUsuario === numeroSecreto) { 
+            // cambios del texto dinamico
             title.textContent = ' Correcto!';
             text.textContent = 'Descubriste el número secreto!';
             trofeo.style.color = '#ffee00'
             inputUsuario.disabled = true;
             buttonUsuario.textContent = 'Reiniciar';
+
         }else if(numeroUsuario < numeroSecreto){
+            // cambios del texto dinamico
             title.textContent = ' Incorrecto!';
             text.textContent = 'El número secreto es MAYOR';
-            inputUsuario.classList.add('error');
+            inputUsuario.classList.add('error'); // agrega animacion error
+
         }else{
+            // cambios del texto dinamico
             title.textContent = ' Incorrecto!';
             text.textContent = 'El número secreto es Menor';
-            inputUsuario.classList.add('error');
+            inputUsuario.classList.add('error'); // agrega animacion error
         }
         
+        // termina animacion error
         setTimeout(() => {
             inputUsuario.classList.remove('error');
         }, 500);
 
+        //selecciona input para cambio constante
         inputUsuario.select();
         contadorIntentos++;
-
+        //texto dinamico
         intentos.textContent = `Quedan ${posiblesIntentos - contadorIntentos} intento${contadorIntentos != 1 ? 's' : ''}`;
 
+        // validacion de termino de intentos
         if (posiblesIntentos - contadorIntentos == 0 && numeroUsuario != numeroSecreto) {
+            // cambios del texto dinamico
             title.textContent = ' Perdiste!';
             text.textContent = 'Vuelve a intentarlo';
             inputUsuario.disabled = true;
-            buttonUsuario.textContent = 'Reiniciar';
+            buttonUsuario.textContent = 'Reiniciar'; // cambia la funcionalidad del boton
         }
     }
 }
 
-// validacion del input para el usuario solo numero del 1 - 10
+// validacion del input para el usuario solo numero del 1 - numero deseado
 document.getElementById('numero-usuario').addEventListener('input', function(event) {
     numeroUsuario = this;
     // Remover cualquier carácter que no sea un número
